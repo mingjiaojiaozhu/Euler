d47a72deea1db3914f1aa60c126d3a2e855d87ee
mingjiaojiaozhu <liuxuming_0613@163.com>
2023-06-28 18:00:40 +0800

    add

--- project_euler/Problem093/Problem093.go
+++ project_euler/Problem093/Problem093.go
@@ -6,24 +6,24 @@ import (
 )
 
 func Problem093() {
-    signs := []string{"+", "-", "*", "/"}
-    operations := make([][]string, 0)
+    signs := []byte{'+', '-', '*', '/'}
+    operations := make([][]byte, 0)
     getOperations(signs, &operations)
 
     elements := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-    auxiliary := make([]int, 4)
+    auxiliary := make([]float64, 4)
     result := Arithmetic{0, 0}
     calculations := make(map[int]bool)
     combination(elements, 0, 4, auxiliary, 0, operations, &result, calculations)
     fmt.Println(result.value)
 }
 
-func getOperations(signs []string, operations *[][]string) {
-    auxiliary := make([]string, 3)
+func getOperations(signs []byte, operations *[][]byte) {
+    auxiliary := make([]byte, 3)
     search(signs, 3, operations, auxiliary, 0)
 }
 
-func combination(elements []int, index int, count int, auxiliary []int, size int, operations [][]string, pivot *Arithmetic, calculations map[int]bool) {
+func combination(elements []int, index int, count int, auxiliary []float64, size int, operations [][]byte, pivot *Arithmetic, calculations map[int]bool) {
     if count == size {
         calculations = make(map[int]bool)
         permutation(auxiliary, 0, count, operations, calculations)
@@ -36,23 +36,23 @@ func combination(elements []int, index int, count int, auxiliary []int, size int
             pivot.border = border
             pivot.value = 0
             for _, value := range auxiliary {
-                pivot.value = pivot.value * 10 + value
+                pivot.value = pivot.value * 10 + int(value)
             }
         }
         return
     }
 
     for i, value := range elements[index:] {
-        auxiliary[size] = value
+        auxiliary[size] = float64(value)
         size++
         combination(elements, index + i + 1, count, auxiliary, size, operations, pivot, calculations)
         size--
     }
 }
 
-func search(signs []string, count int, operations *[][]string, auxiliary []string, size int) {
+func search(signs []byte, count int, operations *[][]byte, auxiliary []byte, size int) {
     if count == size {
-        *operations = append(*operations, make([]string, count))
+        *operations = append(*operations, make([]byte, count))
         copy((*operations)[len(*operations) - 1], auxiliary)
         return
     }
@@ -65,15 +65,15 @@ func search(signs []string, count int, operations *[][]string, auxiliary []strin
     }
 }
 
-func permutation(elements []int, index int, length int, operations [][]string, calculations map[int]bool) {
+func permutation(elements []float64, index int, length int, operations [][]byte, calculations map[int]bool) {
     if index == length {
         for _, operation := range operations {
-            addCalculations(calculate(operation[2], calculate(operation[1], calculate(operation[0], float64(elements[0]), float64(elements[1])), float64(elements[2])), float64(elements[3])), calculations)
-            addCalculations(calculate(operation[1], calculate(operation[0], float64(elements[0]), float64(elements[1])), calculate(operation[2], float64(elements[2]), float64(elements[3]))), calculations)
-            addCalculations(calculate(operation[2], calculate(operation[0], float64(elements[0]), calculate(operation[1], float64(elements[1]), float64(elements[2]))), float64(elements[3])), calculations)
-            addCalculations(calculate(operation[0], float64(elements[0]), calculate(operation[2], calculate(operation[1], float64(elements[1]), float64(elements[2])), float64(elements[3]))), calculations)
-            addCalculations(calculate(operation[0], float64(elements[0]), calculate(operation[1], float64(elements[1]), calculate(operation[2], float64(elements[2]), float64(elements[3])))), calculations)
-            addCalculations(calculate(operation[0], float64(elements[0]), calculate(operation[1], float64(elements[1]), calculate(operation[2], float64(elements[2]), float64(elements[3])))), calculations)
+            addCalculations(calculate(operation[2], calculate(operation[1], calculate(operation[0], elements[0], elements[1]), elements[2]), elements[3]), calculations)
+            addCalculations(calculate(operation[1], calculate(operation[0], elements[0], elements[1]), calculate(operation[2], elements[2], elements[3])), calculations)
+            addCalculations(calculate(operation[2], calculate(operation[0], elements[0], calculate(operation[1], elements[1], elements[2])), elements[3]), calculations)
+            addCalculations(calculate(operation[0], elements[0], calculate(operation[2], calculate(operation[1], elements[1], elements[2]), elements[3])), calculations)
+            addCalculations(calculate(operation[0], elements[0], calculate(operation[1], elements[1], calculate(operation[2], elements[2], elements[3]))), calculations)
+            addCalculations(calculate(operation[0], elements[0], calculate(operation[1], elements[1], calculate(operation[2], elements[2], elements[3]))), calculations)
         }
         return
     }
@@ -92,14 +92,14 @@ func addCalculations(value float64, calculations map[int]bool) {
     }
 }
 
-func calculate(sign string, i float64, j float64) float64 {
-    if "+" == sign {
+func calculate(sign byte, i float64, j float64) float64 {
+    if '+' == sign {
         return i + j
     }
-    if "-" == sign {
+    if '-' == sign {
         return i - j
     }
-    if "*" == sign {
+    if '*' == sign {
         return i * j
     }
     if math.Abs(j) < 0.00000001 {
